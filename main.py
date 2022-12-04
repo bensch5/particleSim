f73fe5c439f0dccdcd403b551b20526e64adef87
Benjamin Schröder <Benjamin.Schroeder@haw-hamburg.de>
2022-12-04 21:22:46 +0100

    implement basic sand simulation

--- main.py
+++ main.py
@@ -17,6 +17,37 @@ def process_input(tiles, x, y):
             tiles[x][y] = Particle(2, x, y)
 
 
+def do_simulation_step(tiles, display_tiles):
+    for x, ary in enumerate(tiles):
+        for y, p in enumerate(ary):
+            match p.p_id:
+                case 0:
+                    pass
+                case 1:
+                    simulate_sand(tiles, display_tiles, p, len(tiles) - 2, len(ary) - 1)
+
+
+def simulate_sand(tiles, display_tiles, particle, max_x, max_y):
+    y = particle.pos_y
+    x = particle.pos_x
+    if y < max_y:
+        if tiles[x][y + 1].p_id == 0:  # directly below
+            display_tiles[x][y] = Particle(0, x, y)
+            display_tiles[x][y + 1] = Particle(particle.p_id, x, y + 1)
+        elif x > 0 and tiles[x - 1][y + 1].p_id == 0:  # bottom left
+            display_tiles[x][y] = Particle(0, x, y)
+            display_tiles[x - 1][y + 1] = Particle(particle.p_id, x - 1, y + 1)
+        elif x < max_x and tiles[x+1][y+1].p_id == 0:  # bottom right
+            display_tiles[x][y] = Particle(0, x, y)
+            display_tiles[x + 1][y + 1] = Particle(particle.p_id, x + 1, y + 1)
+
+
+def display_frame(display_tiles):
+    for x, ary in enumerate(display_tiles):
+        for y, p in enumerate(ary):
+            rl.DrawRectangle(x * GRID_SCALE + 1, y * GRID_SCALE + 1, GRID_SCALE, GRID_SCALE, p.color)
+
+
 def main():
     init_window(G_WIDTH * GRID_SCALE, G_HEIGHT * GRID_SCALE, "Window")
     set_target_fps(FPS)
@@ -32,12 +63,12 @@ def main():
         clear_background(BLACK)
         pos_x = (get_mouse_x() / GRID_SCALE).__floor__()
         pos_y = (get_mouse_y() / GRID_SCALE).__floor__()
-        process_input(tiles, pos_x, pos_y)
-
-        for x, ary in enumerate(tiles):
-            for y, p in enumerate(ary):
-                if p.p_id != 0:
-                    rl.DrawRectangle(x * GRID_SCALE + 1, y * GRID_SCALE + 1, GRID_SCALE, GRID_SCALE, p.color)
+        if rl.IsMouseButtonDown(0) or rl.IsMouseButtonDown(1):
+            process_input(tiles, pos_x, pos_y)
+        display_tiles = tiles.copy()
+        do_simulation_step(tiles, display_tiles)
+        display_frame(display_tiles)
+        tiles = display_tiles.copy()
 
         # show indicator for current drawing position
         rl.DrawRectangle(pos_x * GRID_SCALE + 1, pos_y * GRID_SCALE + 1, GRID_SCALE, GRID_SCALE, RED)
--- particle.py
+++ particle.py
@@ -5,14 +5,20 @@ from pyray import *
 class Particle:
     def __init__(self, p_id, pos_x, pos_y):
         match p_id:
-            case 0:
-                self.color = None
-            case 1:
+            case 0:  # empty tile
+                self.color = BLACK
+            case 1:  # sand
                 self.color = YELLOW
-            case 2:
+            case 2:  # water
                 self.color = BLUE
             case _:
                 raise ValueError('Wrong particle ID. No such particle exists.')
         self.p_id = p_id
         self.pos_x = pos_x
         self.pos_y = pos_y
+
+    def __repr__(self):
+        match self.p_id:
+            case 0: return ' '
+            case 1: return 'sand'
+            case 2: return 'water'
